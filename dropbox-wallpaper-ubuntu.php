d510ca47c2146a16734c3d18db14546abb991b45
Dean Rather <deanrather@gmail.com>
2015-06-08 01:35:18 +1000

    Update dropbox-wallpaper-ubuntu.php

--- dropbox-wallpaper-ubuntu.php
+++ dropbox-wallpaper-ubuntu.php
@@ -14,15 +14,18 @@
 $URL = $argv[1];
 echo "Getting Photo List from: $URL\n";
 $html = file_get_contents($URL);
+if(!$html) { echo "Failed :(\n"; exit(1); }
 
 $json = explode("(function (dropbox) { var SharingModel = dropbox.SharingModel;SharingModel.init_folder(true, true, ", $html);
 $json = $json[1];
 $json = explode(") }(dropbox));", $json);
 $json = $json[0];
 $json = json_decode($json);
+if(!$json) { echo "Failed :(\n"; exit(2); }
 
 $photoIndex = rand(0, sizeof($json)-1);
 $photoURL = $json[$photoIndex]->dl_url;
+if(!$json) { echo "Failed :(\n"; exit(3); }
 
 $path = $_SERVER['HOME'] . '/.wallpapers/' . basename($photoURL);
 $path = explode('?', $path);
@@ -32,9 +35,12 @@ $path = urldecode($path);
 if(!file_exists($path))
 {
 	echo "Getting Photo from: $photoURL\n";
-	exec("mkdir -p ~/.wallpapers");
-	exec("wget -qO '$path' '$photoURL'");
+	exec("mkdir -p ~/.wallpapers", $output, $returnCode);
+	if($returnCode !== 0) { echo "Failed :(\n"; exit(4); }
+	exec("wget -qO '$path' '$photoURL'", $output, $returnCode);
+	if($returnCode !== 0) { echo "Failed :(\n"; exit(5); }
 }
 
 echo "Setting wallpaper to: $path\n";
-exec("gsettings set org.gnome.desktop.background picture-uri 'file:///$path'");
+exec("gsettings set org.gnome.desktop.background picture-uri 'file:///$path'", $output, $returnCode);
+if($returnCode !== 0) { echo "Failed :(\n"; exit(6); }
